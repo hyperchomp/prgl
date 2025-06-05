943dfc2eabe918f04fd2ea1a0dfcb018d6adbc15
hyperchomp <c.lintonrbrown@gmail.com>
2025-06-04 20:55:04 -0600

    Added an array length macro, added additional guards to common macros

--- include/common_macros.h
+++ include/common_macros.h
@@ -2,15 +2,26 @@
 #define PR3D_COMMON_MACROS_H
 
 #ifdef __GNUC__
+#ifndef UNUSED
 #define UNUSED(x) UNUSED_##x __attribute__((__unused__))
+#endif
 #else
+#ifndef UNUSED
 #define UNUSED(x) UNUSED_##x
 #endif
-
-#ifdef __GNUC__
-#define UNUSED_FUNCTION(x) __attribute__((__unused__)) UNUSED_##x
-#else
-#define UNUSED_FUNCTION(x) UNUSED_##x
 #endif
 
+/**
+ * Can be used to get the length of an array when used in the same scope it is
+ * declared in. This version has some protections from passing pointers as
+ * arguments, but it isn't completely foolproof.
+ *
+ * @param[in] x The array to get the length of
+ * @return size_t The length of the array
+ */
+#ifndef ARR_LEN
+#define ARR_LEN(x)                                                             \
+    ((size_t)(sizeof(x) / sizeof(0 [x])) /                                     \
+     ((size_t)(!(sizeof(x) % sizeof(0 [x])))))
+#endif
 #endif
