167c032223d164a1ec3c58cb3e3a80ff4c1d326b
hyperchomp <c.lintonrbrown@gmail.com>
2025-07-11 17:54:32 -0600

    Added camera struct members and directional movement code

--- include/camera.h
+++ include/camera.h
@@ -10,10 +10,29 @@
  */
 struct PR3DCamera
 {
-    mat4 position;   ///< The position of the camera stored as a view matrix.
+    mat4 view; ///< The position of the camera stored as a view matrix.
     mat4 projection; ///< The camera's projection matrix for fov, clipping, etc.
 
+    vec3 position;
+    vec3 front;
+    vec3 right;
+    vec3 up;
+
+    float yaw;
+    float pitch;
     float fov; ///< Camera fov in degrees
+    float move_speed;
+};
+
+/**
+ * Camera movement directions to obfuscate movement from input.
+ */
+enum PR3DCameraMoveDirection
+{
+    PR3D_CAMERA_FORWARD,
+    PR3D_CAMERA_BACKWARD,
+    PR3D_CAMERA_RIGHT,
+    PR3D_CAMERA_LEFT
 };
 
 /**
@@ -22,8 +41,13 @@ struct PR3DCamera
  *
  * @param cam[in,out] An uninitialized PR3DCamera struct.
  * @param fov_degrees The vertical fov for the camera in degrees.
+ * @param move_speed Speed value to use when moving the camera.
  */
-void pr3d_init_camera(struct PR3DCamera *cam, float fov_degrees);
+void pr3d_init_camera(
+    struct PR3DCamera *cam,
+    float fov_degrees,
+    float move_speed
+);
 
 /**
  * Sends the view and projection matrices to the shader every frame.
@@ -33,12 +57,16 @@ void pr3d_init_camera(struct PR3DCamera *cam, float fov_degrees);
 void pr3d_update_camera(struct PR3DCamera *cam);
 
 /**
- * Moves the camera to a new position.
+ * Smoothly moves the camera in a given direction using its speed.
  *
  * @param cam[in,out]
- * @param position
+ * @param move_dir
+ * @param delta_time
  */
-void pr3d_move_camera(struct PR3DCamera *cam, vec3 position);
+void pr3d_move_camera(
+    struct PR3DCamera *cam, enum PR3DCameraMoveDirection move_dir,
+    double delta_time
+);
 
 /**
  * Updates the camera's vertical fov.
--- src/camera.c
+++ src/camera.c
@@ -2,27 +2,90 @@
 #include "screen_internal.h"
 #include "shaders.h"
 #include <cglm/cglm.h>
-#include <cglm/mat4.h>
-#include <cglm/vec3.h>
 
-void pr3d_init_camera(struct PR3DCamera *cam, float fov_degrees)
+static vec3 PR3D_WORLD_UP = {0.0f, 1.0f, 0.0f};
+
+void pr3d_init_camera(
+    struct PR3DCamera *cam, float fov_degrees, float move_speed
+)
 {
-    pr3d_move_camera(cam, GLM_VEC3_ZERO);
+    glm_vec3_zero(cam->position);
+    glm_vec3_zero(cam->up);
+    glm_vec3_zero(cam->front);
+    glm_vec3_zero(cam->right);
+
+    cam->up[1] = 1.0f;
+    cam->front[2] = -1.0f;
+    cam->yaw = -90.0f;
+    cam->pitch = 0.0f;
+    cam->move_speed = move_speed;
+
     pr3d_set_camera_fov(cam, fov_degrees);
 }
 
 void pr3d_update_camera(struct PR3DCamera *cam)
 {
-    // The view matrix is the camera position
+    // Calculate front vector, this is the z-axis
+    vec3 front;
+    const float yaw_rads = glm_rad(cam->yaw);
+    const float pitch_rads = glm_rad(cam->pitch);
+    const float cos_pitch = cosf(pitch_rads);
+    front[0] = cosf(yaw_rads) * cos_pitch;
+    front[1] = sinf(pitch_rads);
+    front[2] = sinf(yaw_rads) * cos_pitch;
+    glm_normalize(front);
+    glm_vec3_copy(front, cam->front);
+
+    // Right and Up vector as well
+    glm_cross(cam->front, PR3D_WORLD_UP, cam->right); // This is the x-axis
+    glm_normalize(cam->right);
+
+    glm_cross(cam->right, cam->front, cam->up); // This is the y-axis
+    glm_normalize(cam->up);
+
+    // Now calculate the direction of the camera, and adjust the view matrix
+    vec3 direction;
+    glm_vec3_add(cam->position, cam->front, direction);
+    glm_lookat(cam->position, direction, cam->up, cam->view);
+
+    // The view matrix is the camera position, which we calculated above
     pr3d_set_shader_uniform_mat4(
-        pr3d_shader(PR3D_SHADER_TEXTURE), "view", cam->position
+        pr3d_shader(PR3D_SHADER_TEXTURE), "view", cam->view
     );
 }
 
-void pr3d_move_camera(struct PR3DCamera *cam, vec3 position)
+void pr3d_move_camera(
+    struct PR3DCamera *cam, const enum PR3DCameraMoveDirection move_dir,
+    const double delta_time
+)
 {
-    glm_mat4_identity(cam->position);
-    glm_translate(cam->position, position);
+    const float velocity = cam->move_speed * (float)delta_time;
+    vec3 front_velocity;
+    glm_vec3_scale(cam->front, velocity, front_velocity);
+    vec3 right_velocity;
+    glm_vec3_scale(cam->right, velocity, right_velocity);
+
+    switch (move_dir)
+    {
+        case PR3D_CAMERA_FORWARD:
+            glm_vec3_add(cam->position, front_velocity, cam->position);
+            break;
+        case PR3D_CAMERA_BACKWARD:
+            glm_vec3_sub(cam->position, front_velocity, cam->position);
+            break;
+        case PR3D_CAMERA_LEFT:
+            glm_vec3_sub(cam->position, right_velocity, cam->position);
+            break;
+        case PR3D_CAMERA_RIGHT:
+            glm_vec3_add(cam->position, right_velocity, cam->position);
+            break;
+        default:
+            fprintf(
+                stderr,
+                "pr3d_move_camera: Unspecified camera move direction type\n"
+            );
+            exit(EXIT_FAILURE);
+    }
 }
 
 void pr3d_set_camera_fov(struct PR3DCamera *cam, float fov_degrees)
