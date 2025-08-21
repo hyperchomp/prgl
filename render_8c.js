44e6c929f8d6e2e311a4c462da27d57bb75b8152
hyperchomp <c.lintonrbrown@gmail.com>
2025-08-21 20:35:39 +0000

    Deploying to gh-pages from @ hyperchomp/pr3d@c9f5e1f0da1149d200a026d59a8dd83abd90196e 🚀

--- render_8c.js
+++ render_8c.js
@@ -3,9 +3,7 @@ var render_8c =
     [ "pr3d_clear_screen", "render_8c.html#a4c8390e54cd21087f02961f292abab02", null ],
     [ "pr3d_create_cube", "render_8c.html#a196141e1b4bb71847f218c9f2870e233", null ],
     [ "pr3d_create_rectangle", "render_8c.html#aaa4b8e7639ef07fe468dbe4932fb8674", null ],
-    [ "pr3d_create_rectangle_vertex_color", "render_8c.html#a4b666470b0d3d177966354f971ee05d6", null ],
     [ "pr3d_create_triangle", "render_8c.html#a331cc7b8d6872db50536ac46504dafc8", null ],
-    [ "pr3d_create_triangle_vertex_color", "render_8c.html#a75a041971949d5716e3b123a90939f5d", null ],
     [ "pr3d_delete_mesh", "render_8c.html#a2e2eae802cf220b32b263c65a4ea8823", null ],
     [ "pr3d_render_mesh", "render_8c.html#a2c3c6351ac03485f63485d220ba69ce2", null ],
     [ "pr3d_render_mesh_2d", "render_8c.html#a8a2b878f2f08c87cd356780d382e4959", null ]
