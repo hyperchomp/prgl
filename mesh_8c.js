7d698cbab9c9e2e410f4f9d70642f5090c2101af
hyperchomp <c.lintonrbrown@gmail.com>
2025-08-25 15:53:31 +0000

    Deploying to gh-pages from @ hyperchomp/pr3d@d427cdd255ea7dc99da121dacf8f69806150f6b8 🚀

--- mesh_8c.js
+++ mesh_8c.js
@@ -2,7 +2,7 @@ var mesh_8c =
 [
     [ "pr3d_calculate_aabb", "mesh_8c.html#a25f8b0c69be593e730626f6b514827bc", null ],
     [ "pr3d_create_cube", "mesh_8c.html#a196141e1b4bb71847f218c9f2870e233", null ],
-    [ "pr3d_create_rectangle", "mesh_8c.html#a6012977a189847a4a83c2d27843a54ed", null ],
+    [ "pr3d_create_quad", "mesh_8c.html#a181e479882d3054c5a409fef19aa3484", null ],
     [ "pr3d_create_triangle", "mesh_8c.html#a331cc7b8d6872db50536ac46504dafc8", null ],
     [ "pr3d_delete_mesh", "mesh_8c.html#a2e2eae802cf220b32b263c65a4ea8823", null ]
 ];
--- mesh_8h.js
+++ mesh_8h.js
@@ -3,7 +3,7 @@ var mesh_8h =
     [ "PR3DMesh", "structPR3DMesh.html", "structPR3DMesh" ],
     [ "pr3d_calculate_aabb", "mesh_8h.html#a25f8b0c69be593e730626f6b514827bc", null ],
     [ "pr3d_create_cube", "mesh_8h.html#a196141e1b4bb71847f218c9f2870e233", null ],
-    [ "pr3d_create_rectangle", "mesh_8h.html#a6012977a189847a4a83c2d27843a54ed", null ],
+    [ "pr3d_create_quad", "mesh_8h.html#a181e479882d3054c5a409fef19aa3484", null ],
     [ "pr3d_create_triangle", "mesh_8h.html#a331cc7b8d6872db50536ac46504dafc8", null ],
     [ "pr3d_delete_mesh", "mesh_8h.html#a2e2eae802cf220b32b263c65a4ea8823", null ]
 ];
