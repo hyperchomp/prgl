2992ca2cba3b71f840500b9ed5a4c0ef1dd95fdd
hyperchomp <c.lintonrbrown@gmail.com>
2025-08-30 03:41:19 +0000

    Deploying to gh-pages from @ hyperchomp/prgl@e5076db4d8bb003c48691b4e42e74e18430efe9e 🚀

--- game_8c.js
+++ game_8c.js
@@ -1,7 +1,7 @@
 var game_8c =
 [
     [ "prgl_delta_time", "game_8c.html#a157b31b69e21c0add3f3f9616ce7eeea", null ],
-    [ "prgl_run_game", "game_8c.html#acddb7017dceb9e3bfc9bc7b5ac66318e", null ],
+    [ "prgl_run_game", "game_8c.html#ade7c2bc974a3632ad521b2f6c889ed8d", null ],
     [ "render_texture", "game_8c.html#a373f5884865a5eefc0ef2c33ffa8c6b8", null ],
     [ "screen_render_quad", "game_8c.html#a4d74b20a8ca15b0b1b7d9822ee585fec", null ]
 ];
--- game_8h.js
+++ game_8h.js
@@ -1,5 +1,5 @@
 var game_8h =
 [
     [ "prgl_delta_time", "game_8h.html#a157b31b69e21c0add3f3f9616ce7eeea", null ],
-    [ "prgl_run_game", "game_8h.html#acddb7017dceb9e3bfc9bc7b5ac66318e", null ]
+    [ "prgl_run_game", "game_8h.html#ade7c2bc974a3632ad521b2f6c889ed8d", null ]
 ];
