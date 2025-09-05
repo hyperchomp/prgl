10118cfef50d4d3cf6fcaaf2034b034da72c9595
hyperchomp <c.lintonrbrown@gmail.com>
2025-09-04 22:26:40 -0600

    Added cube sphere mesh creation, refactored mesh code to be more modular

--- include/mesh.h
+++ include/mesh.h
@@ -4,8 +4,9 @@
 #include "cglm/types.h"
 
 /**
- * Defines the geometry of a 3D object. Only one Mesh is needed to draw many
- * of the same object.
+ * @brief Defines the geometry of a 3D object.
+ *
+ * One mesh can be re-used to render many of the same object.
  * Any IDs which are optional should be set to 0 if unused.
  */
 struct PRGLMesh
@@ -26,7 +27,7 @@ struct PRGLMesh
 };
 
 /**
- * Creates a triangle mesh with the given vertex positions.
+ * @brief Creates a triangle mesh with the given vertex positions.
  *
  * @param[in] vertices
  */
@@ -41,13 +42,24 @@ struct PRGLMesh *prgl_create_triangle(mat3 vertices);
 struct PRGLMesh *prgl_create_quad(mat4 vertices);
 
 /**
- * Creates a cube mesh.
+ * @brief Creates a cube mesh.
  * Make sure to attach a texture to the returned mesh struct after creating it.
  */
 struct PRGLMesh *prgl_create_cube(void);
 
 /**
- * Cleans up the GL objects associated with the mesh and frees it.
+ * @brief Creates a cube sphere.
+ * Generates a sphere constructed from a cube by subdividing and expanding the
+ * six faces.
+ *
+ * @param resolution The resolution for the subdivision of each face. The number
+ * of quads per face will be this value squared, for example if resolution is 2
+ * there will be 4 quads per face.
+ */
+struct PRGLMesh *prgl_create_cube_sphere(int resolution);
+
+/**
+ * @brief Cleans up the GL objects associated with the mesh and frees it.
  *
  * @param mesh[in,out]
  */
--- src/mesh.c
+++ src/mesh.c
@@ -3,8 +3,23 @@
 #include "glad.h"
 #include "common_macros.h"
 #include "cglm/types.h"
+#include "cglm/vec3.h"
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+// Constants for meshes with vertices, normals, and texture coordinates. If not
+// all then these values will be different, only use in that scenario.
+static const int VERTEX_STRIDE_LENGTH = 8;
+static const GLint FLOAT_VERTEX_STRIDE = VERTEX_STRIDE_LENGTH * sizeof(float);
+static const GLint NORMALS_OFFSET = 3 * sizeof(float);
+static const GLint TEX_COORD_OFFSET = 6 * sizeof(float);
+
+static void prgl_setup_vertex_attributes(void);
+static void prgl_generate_cube_sphere_point(
+    vec3 point, float u, float v, vec3 face_right, vec3 face_up,
+    vec3 face_normal, vec3 quad_right, vec3 quad_up
+);
 
 void prgl_init_mesh(
     struct PRGLMesh *mesh, int num_vertices, unsigned int vao, unsigned int vbo,
@@ -84,15 +99,18 @@ struct PRGLMesh *prgl_create_screen_quad(void)
 
     // Tell OpenGL how to interpret the texture coordinate data
     glVertexAttribPointer(
-        2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)(3 * sizeof(float))
+        2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
+        (const GLvoid *)(intptr_t)(3 * sizeof(float))
     );
     glEnableVertexAttribArray(2);
 
     struct PRGLMesh *mesh = malloc(sizeof(struct PRGLMesh));
     if (mesh == NULL)
     {
-        printf("prgl_create_screen_quad: Error allocating mesh "
-               "pointer memory!");
+        fprintf(
+            stderr, "prgl_create_screen_quad: Error allocating mesh "
+                    "pointer memory!"
+        );
     }
 
     prgl_init_mesh(mesh, ARR_LEN(indices), vao, vbo, ebo);
@@ -141,14 +159,18 @@ struct PRGLMesh *prgl_create_triangle(mat3 vertices)
 
     // texture coord attribute
     glVertexAttribPointer(
-        2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)(3 * sizeof(float))
+        2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
+        (const GLvoid *)(intptr_t)(3 * sizeof(float))
     );
     glEnableVertexAttribArray(2);
 
     struct PRGLMesh *mesh = malloc(sizeof(struct PRGLMesh));
     if (mesh == NULL)
     {
-        printf("prgl_create_triangle: Error allocating mesh pointer memory!");
+        fprintf(
+            stderr,
+            "prgl_create_triangle: Error allocating mesh pointer memory!"
+        );
     }
 
     prgl_init_mesh(mesh, 3, vao, vbo, 0);
@@ -212,29 +234,15 @@ struct PRGLMesh *prgl_create_quad(mat4 vertices)
         GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW
     );
 
-    // Tell OpenGL how to interpret the vertex data
-    glVertexAttribPointer(
-        0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0
-    );
-    glEnableVertexAttribArray(0);
-
-    // Tell OpenGL how to interpret the normal data
-    glVertexAttribPointer(
-        1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float))
-    );
-    glEnableVertexAttribArray(1);
-
-    // Tell OpenGL how to interpret the texture coordinate data
-    glVertexAttribPointer(
-        2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float))
-    );
-    glEnableVertexAttribArray(2);
+    prgl_setup_vertex_attributes();
 
     struct PRGLMesh *mesh = malloc(sizeof(struct PRGLMesh));
     if (mesh == NULL)
     {
-        printf("prgl_create_rectangle: Error allocating mesh "
-               "pointer memory!");
+        fprintf(
+            stderr, "prgl_create_rectangle: Error allocating mesh "
+                    "pointer memory!"
+        );
     }
 
     prgl_init_mesh(mesh, ARR_LEN(indices), vao, vbo, ebo);
@@ -299,31 +307,162 @@ struct PRGLMesh *prgl_create_cube(void)
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    // position attribute
-    glVertexAttribPointer(
-        0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0
-    );
-    glEnableVertexAttribArray(0);
-
-    // normals attribute
-    glVertexAttribPointer(
-        1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float))
-    );
-    glEnableVertexAttribArray(1);
-
-    // texture coord attribute
-    glVertexAttribPointer(
-        2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float))
-    );
-    glEnableVertexAttribArray(2);
+    prgl_setup_vertex_attributes();
 
     struct PRGLMesh *mesh = malloc(sizeof(struct PRGLMesh));
     if (mesh == NULL)
     {
-        printf("prgl_create_cube: Error allocating mesh pointer memory!");
+        fprintf(
+            stderr, "prgl_create_cube: Error allocating mesh pointer memory!"
+        );
     }
 
     prgl_init_mesh(mesh, 36, vao, vbo, 0);
+    return mesh;
+}
+
+struct PRGLMesh *prgl_create_cube_sphere(int resolution)
+{
+    resolution = resolution > 1 ? resolution : 1;
+
+    // Six faces, resolution squared quads, six vertices per quad
+    const int NUM_FACES = 6;
+    int num_vertices = NUM_FACES * (resolution * resolution) * NUM_FACES;
+    int vertex_data_length = num_vertices * VERTEX_STRIDE_LENGTH;
+
+    float *vertex_data = malloc(sizeof(float) * vertex_data_length);
+    if (vertex_data == NULL)
+    {
+        fprintf(
+            stderr,
+            "prgl_create_cube_sphere: Error allocating vertex data memory!"
+        );
+    }
+
+    // clang-format off
+    vec3 face_normals[6] = {
+        { 0.0f,  0.0f,  1.0f}, // front
+        { 0.0f,  0.0f, -1.0f}, // back
+        { 0.0f,  1.0f,  0.0f}, // up
+        { 0.0f, -1.0f,  0.0f}, // down
+        { 1.0f,  0.0f,  0.0f}, // right
+        {-1.0f,  0.0f,  0.0f},  // left
+    };
+
+    vec3 face_rights[6] = {
+        { 1.0f,  0.0f,  0.0f}, // front
+        {-1.0f,  0.0f,  0.0f}, // back
+        { 1.0f,  0.0f,  0.0f}, // up
+        { 1.0f,  0.0f,  0.0f}, // down
+        { 0.0f,  0.0f, -1.0f}, // right
+        { 0.0f,  0.0f,  1.0f},  // left
+    };
+    // clang-format on
+
+    int component_index = 0;
+    for (int face = 0; face < NUM_FACES; face++)
+    {
+        vec3 normal;
+        glm_vec3_copy(face_normals[face], normal);
+
+        vec3 right;
+        glm_vec3_copy(face_rights[face], right);
+
+        vec3 up;
+        glm_vec3_cross(normal, right, up);
+
+        // Iterate quads for the current face
+        for (int y = 0; y < resolution; y++)
+        {
+            for (int x = 0; x < resolution; x++)
+            {
+                // Texture UV coordinate values, the plus ones are to fix seams
+                float u = (float)x / resolution;
+                float v = (float)y / resolution;
+                float u1 = (float)(x + 1) / resolution;
+                float v1 = (float)(y + 1) / resolution;
+
+                // Four corner points on the current quad
+                vec3 points[4];
+                vec3 quad_right;
+                vec3 quad_up;
+
+                prgl_generate_cube_sphere_point(
+                    points[0], u, v, right, up, normal, quad_right, quad_up
+                );
+                prgl_generate_cube_sphere_point(
+                    points[1], u1, v, right, up, normal, quad_right, quad_up
+                );
+                prgl_generate_cube_sphere_point(
+                    points[2], u1, v1, right, up, normal, quad_right, quad_up
+                );
+                prgl_generate_cube_sphere_point(
+                    points[3], u, v1, right, up, normal, quad_right, quad_up
+                );
+
+                // Create two triangles from the points to form a quad
+                vec3 triangle_points[6] = {
+                    {points[0][0], points[0][1], points[0][2]},
+                    {points[1][0], points[1][1], points[1][2]},
+                    {points[2][0], points[2][1], points[2][2]},
+                    {points[0][0], points[0][1], points[0][2]},
+                    {points[2][0], points[2][1], points[2][2]},
+                    {points[3][0], points[3][1], points[3][2]},
+                };
+                vec2 uvs[6] = {
+                    {u, v}, {u1, v}, {u1, v1}, {u, v}, {u1, v1}, {u, v1},
+                };
+
+                // Normalize and project the points to vertices on the sphere
+                for (int v = 0; v < 6; v++)
+                {
+                    vec3 position;
+                    glm_vec3_normalize_to(triangle_points[v], position);
+
+                    // Position XYZ coordinates
+                    vertex_data[component_index++] = position[0];
+                    vertex_data[component_index++] = position[1];
+                    vertex_data[component_index++] = position[2];
+
+                    // Normal XYZ coordinates
+                    vertex_data[component_index++] = position[0];
+                    vertex_data[component_index++] = position[1];
+                    vertex_data[component_index++] = position[2];
+
+                    // Texture UV coordinates
+                    vertex_data[component_index++] = uvs[v][0];
+                    vertex_data[component_index++] = uvs[v][1];
+                }
+            }
+        }
+    }
+
+    unsigned int vbo;
+    unsigned int vao;
+    glGenBuffers(1, &vbo);
+    glGenVertexArrays(1, &vao);
+
+    glBindVertexArray(vao);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(
+        GL_ARRAY_BUFFER, sizeof(float) * vertex_data_length, vertex_data,
+        GL_STATIC_DRAW
+    );
+
+    prgl_setup_vertex_attributes();
+    free(vertex_data);
+
+    struct PRGLMesh *mesh = malloc(sizeof(struct PRGLMesh));
+    if (mesh == NULL)
+    {
+        fprintf(
+            stderr,
+            "prgl_create_cube_sphere: Error allocating mesh pointer memory!"
+        );
+    }
+
+    prgl_init_mesh(mesh, num_vertices, vao, vbo, 0);
     return mesh;
 }
 
@@ -338,4 +477,42 @@ void prgl_delete_mesh(struct PRGLMesh *mesh)
     }
 
     free(mesh);
+}
+
+static void prgl_setup_vertex_attributes(void)
+{
+    // position attribute
+    glVertexAttribPointer(
+        0, 3, GL_FLOAT, GL_FALSE, FLOAT_VERTEX_STRIDE, (void *)0
+    );
+    glEnableVertexAttribArray(0);
+
+    // normals attribute
+    glVertexAttribPointer(
+        1, 3, GL_FLOAT, GL_FALSE, FLOAT_VERTEX_STRIDE,
+        (const GLvoid *)(intptr_t)(NORMALS_OFFSET)
+    );
+    glEnableVertexAttribArray(1);
+
+    // texture coord attribute
+    glVertexAttribPointer(
+        2, 2, GL_FLOAT, GL_FALSE, FLOAT_VERTEX_STRIDE,
+        (const GLvoid *)(intptr_t)TEX_COORD_OFFSET
+    );
+    glEnableVertexAttribArray(2);
+}
+
+/**
+ * Used to generate each point for a quad while generating a cube sphere.
+ */
+static void prgl_generate_cube_sphere_point(
+    vec3 point, float u, float v, vec3 face_right, vec3 face_up,
+    vec3 face_normal, vec3 quad_right, vec3 quad_up
+)
+{
+    glm_vec3_scale(face_right, u - 0.5f, quad_right);
+    glm_vec3_scale(face_up, v - 0.5f, quad_up);
+    glm_vec3_scale(face_normal, 0.5f, point);
+    glm_vec3_add(point, quad_right, point);
+    glm_vec3_add(point, quad_up, point);
 }
